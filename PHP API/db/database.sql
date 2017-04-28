a73bd56d241c2be7588275d3a471574cf19de2b3
minhaz <minhazav@gmail.com>
2017-04-28 20:11:30 +0530

    added php code

---
+++ PHP API/db/database.sql
@@ -0,0 +1,20 @@
+
+create database test /** Creating Database **/
+ 
+use test /** Selecting Database **/
+ 
+create table users(
+   id int(11) primary key auto_increment,
+   unique_id varchar(23) not null unique,
+   name varchar(50) not null,
+   email varchar(100) not null unique,
+   encrypted_password varchar(80) not null,
+   salt varchar(10) not null,
+   created_at datetime,
+   updated_at datetime null
+); /** Creating Users Table **/
+
+create table sessions(
+    user_id int(11) primary key,
+    session_id varchar(32)
+);
---
+++ PHP API/include/DB_Functions.php
@@ -0,0 +1,180 @@
+<?php
+
+/**
+ * @author Ravi Tamada
+ * @link http://www.androidhive.info/2012/01/android-login-and-registration-with-php-mysql-and-sqlite/ Complete tutorial
+ */
+
+class DB_Functions {
+
+    private $conn;
+
+    // constructor
+    function __construct() {
+        require_once 'DB_Connect.php';
+        // connecting to database
+        $db = new Db_Connect();
+        $this->conn = $db->connect();
+    }
+
+    // destructor
+    function __destruct() {
+        
+    }
+
+    /**
+     * Storing new user
+     * returns user details
+     */
+    public function storeUser($name, $email, $password) {
+        $uuid = uniqid('', true);
+        $hash = $this->hashSSHA($password);
+        $encrypted_password = $hash["encrypted"]; // encrypted password
+        $salt = $hash["salt"]; // salt
+
+        $stmt = $this->conn->prepare("INSERT INTO users(unique_id, name, email, encrypted_password, salt, created_at) VALUES(?, ?, ?, ?, ?, NOW())");
+        $stmt->bind_param("sssss", $uuid, $name, $email, $encrypted_password, $salt);
+        $result = $stmt->execute();
+        $stmt->close();
+
+        // check for successful store
+        if ($result) {
+            $stmt = $this->conn->prepare("SELECT * FROM users WHERE email = ?");
+            $stmt->bind_param("s", $email);
+            $stmt->execute();
+            $user = $stmt->get_result()->fetch_assoc();
+            $stmt->close();
+
+            return $user;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Get user by email and password
+     */
+    public function getUserByEmailAndPassword($email, $password) {
+
+        $stmt = $this->conn->prepare("SELECT *, sessions.* FROM users 
+            LEFT JOIN sessions on sessions.user_id = users.id
+            WHERE email = ?");
+
+        $stmt->bind_param("s", $email);
+
+        if ($stmt->execute()) {
+            $user = $stmt->get_result()->fetch_assoc();
+            $stmt->close();
+
+            // verifying user password
+            $salt = $user['salt'];
+            $encrypted_password = $user['encrypted_password'];
+            $hash = $this->checkhashSSHA($salt, $password);
+            // check for password equality
+            if ($encrypted_password == $hash) {
+                // user authentication details are correct
+
+                if ($user['session_id'] == null) {
+                    $user['session_id'] = $this->createSession($user);
+                }
+                return $user;
+            }
+        } else {
+            return NULL;
+        }
+    }
+
+    /**
+     * Get user by email and password
+     */
+    public function getUserBySessionId($session_id) {
+
+        $stmt = $this->conn->prepare("SELECT *, sessions.* FROM users 
+            LEFT JOIN sessions on sessions.user_id = users.id
+            WHERE sessions.session_id = ?");
+
+        $stmt->bind_param("s", $session_id);
+
+        if ($stmt->execute()) {
+            $user = $stmt->get_result()->fetch_assoc();
+            $stmt->close();
+
+            return $user;
+        } else {
+            return NULL;
+        }
+    }
+
+    /**
+     * Check user is existed or not
+     */
+    public function isUserExisted($email) {
+        $stmt = $this->conn->prepare("SELECT email from users WHERE email = ?");
+
+        $stmt->bind_param("s", $email);
+
+        $stmt->execute();
+
+        $stmt->store_result();
+
+        if ($stmt->num_rows > 0) {
+            // user existed 
+            $stmt->close();
+            return true;
+        } else {
+            // user not existed
+            $stmt->close();
+            return false;
+        }
+    }
+
+    /**
+     * Create a session if not exists
+     */
+    public function createSession($user) {
+        $id = $user["id"];
+        $session_id = $this->createSessionID();
+
+        $stmt2 = $this->conn->prepare("INSERT INTO `sessions`(`user_id`, `session_id`) VALUES (?, ?)");
+        $stmt2->bind_param("is", $id, $session_id);
+        $stmt2->execute();
+        $stmt2->close();
+        return $session_id;
+     }
+
+    /**
+     * Get session ID for customer
+     */
+     private function createSessionID() {
+         return md5(uniqid());
+     }
+
+    /**
+     * Encrypting password
+     * @param password
+     * returns salt and encrypted password
+     */
+    public function hashSSHA($password) {
+
+        $salt = sha1(rand());
+        $salt = substr($salt, 0, 10);
+        $encrypted = base64_encode(sha1($password . $salt, true) . $salt);
+        $hash = array("salt" => $salt, "encrypted" => $encrypted);
+        return $hash;
+    }
+
+    /**
+     * Decrypting password
+     * @param salt, password
+     * returns hash string
+     */
+    public function checkhashSSHA($salt, $password) {
+
+        $hash = base64_encode(sha1($password . $salt, true) . $salt);
+
+        return $hash;
+    }
+
+}
+
+?>
